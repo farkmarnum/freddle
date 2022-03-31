896a984ff07787df27e8a3d9fe58c358473542d7
farkmarnum <mark.farnum@gmail.com>
2022-03-31 01:11:54 -0400

    Fix tests, lint

--- src/lib/share.test.ts
+++ src/lib/share.test.ts
@@ -1,18 +1,19 @@
 import { generateEmojiGrid } from './share'
-
-const mockSolutionGetter = jest.fn()
-jest.mock('./words', () => ({
-  ...jest.requireActual('./words'),
-  get solution() {
-    return mockSolutionGetter()
-  },
-}))
+import * as words from './words'
 
 describe('generateEmojiGrid', () => {
-  test('generates grid for ascii', () => {
+  let originalSolutionValue: string
+
+  beforeAll(() => {
+    originalSolutionValue = words.solution
+  })
+
+  test('generates grid', () => {
     const guesses = ['EDCBA', 'VWXYZ', 'ABCDE']
-    const tiles = ['C', 'P', 'A'] // Correct, Present, Absemt
-    mockSolutionGetter.mockReturnValue('ABCDE')
+    const tiles = ['C', 'P', 'A'] // Correct, Present, Absent
+
+    // @ts-ignore
+    words.solution = 'ABCDE'
 
     const grid = generateEmojiGrid(guesses, tiles)
     const gridParts = grid.split('\n')
@@ -20,15 +21,23 @@ describe('generateEmojiGrid', () => {
     expect(gridParts[1]).toBe('AAAAA')
     expect(gridParts[2]).toBe('CCCCC')
   })
+
   test('generates grid for ascii', () => {
     const guesses = ['5️⃣4️⃣3️⃣2️⃣1️⃣', '♠️♥️♦️♣️🔔', '1️⃣2️⃣3️⃣4️⃣5️⃣']
-    const tiles = ['C', 'P', 'A'] // Correct, Present, Absemt
-    mockSolutionGetter.mockReturnValue('1️⃣2️⃣3️⃣4️⃣5️⃣')
+    const tiles = ['C', 'P', 'A'] // Correct, Present, Absent
+
+    // @ts-ignore
+    words.solution = '1️⃣2️⃣3️⃣4️⃣5️⃣'
 
     const grid = generateEmojiGrid(guesses, tiles)
     const gridParts = grid.split('\n')
     expect(gridParts[0]).toBe('PPCPP')
     expect(gridParts[1]).toBe('AAAAA')
     expect(gridParts[2]).toBe('CCCCC')
+  })
+
+  afterAll(() => {
+    // @ts-ignore
+    words.solution = originalSolutionValue
   })
 })
